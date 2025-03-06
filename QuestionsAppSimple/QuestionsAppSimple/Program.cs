90b256dbef7c22bebf53fde91a133fed8fc8a69d
Haris Turko <2310653711@stud.fh-kufstein.ac.at>
2025-03-06 15:06:38 +0100

    commit task 2

--- QuestionsAppSimple/QuestionsAppSimple/Program.cs
+++ QuestionsAppSimple/QuestionsAppSimple/Program.cs
@@ -6,13 +6,42 @@ var builder = WebApplication.CreateBuilder(args);
 // Add Swagger services
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-var app = builder.Build();
-using (var scope = app.Services.CreateScope())
-    scope.ServiceProvider.GetRequiredService<QuestionsContext>().Database.EnsureCreated();
+
 var connectionString = new SqliteConnectionStringBuilder() { DataSource = "Production.db" }.ToString();
 builder.Services.AddDbContext<QuestionsContext>(x => x.UseSqlite(connectionString));
 
-app.MapGet("/", () => "Hello World!");
+var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+    scope.ServiceProvider.GetRequiredService<QuestionsContext>().Database.EnsureCreated();
+
+
+// API Routes
+
+app.MapGet("api/questions", async (QuestionsContext context)
+    => await context.Questions.ToListAsync());
+
+app.MapPost("api/questions/", async (QuestionsContext context, string content) =>
+{
+    if (string.IsNullOrWhiteSpace(content))
+        return Results.BadRequest("The Question Content can not be empty");
+
+    context.Questions.Add(new Question { Content = content });
+    await context.SaveChangesAsync();
+    return Results.Ok();
+});
+
+app.MapPost("api/questions/{id:int}/vote", async (QuestionsContext context, int id) =>
+{
+    var question = await context.Questions.FirstOrDefaultAsync(q => q.Id == id);
+    if (question is null)
+        return Results.BadRequest("Invalid Question Id");
+
+    question.Votes++;
+    await context.SaveChangesAsync();
+    return Results.Ok();
+});
+
 // Enable swagger and SwaggerUI in Developer Mode.
 if (app.Environment.IsDevelopment())
 {
--- QuestionsAppSimple/QuestionsAppSimple/Properties/DB/QuestionsContext.cs
+++ QuestionsAppSimple/QuestionsAppSimple/Properties/DB/QuestionsContext.cs
@@ -4,5 +4,8 @@ namespace QuestionsAppSimple.Properties.DB;
 
 public class QuestionsContext : DbContext
 {
+    public QuestionsContext(DbContextOptions options) : base(options)
+    {
+    }
     public DbSet<Question> Questions { get; set; }
 }
